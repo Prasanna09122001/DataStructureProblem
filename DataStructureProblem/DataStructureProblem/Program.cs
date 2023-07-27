7b7b6210e857f4d8501421c3339f9c8c5d38adee
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-27 10:57:54 +0530

    [Prasanna] Add:Implemented the Binary Search

--- DataStructureProblem/DataStructureProblem/Program.cs
+++ DataStructureProblem/DataStructureProblem/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DataStructureProblem.Files;
+
+class program
+{
+    static string filepath = @"D:\Bridgelabz Statement\DataStructureProblem\DataStructureProblem\DataStructureProblem\DataStructureProblem\Files\Search.txt";
+    static void Main()
+    {
+        bool flag = true;
+        while (flag)
+        {
+            Console.WriteLine("Enter the option to Execute\n1.Binary Search\n2.Exit");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Welcome to Data Structure Problem");
+                    BinarySearch binarySearch = new BinarySearch();
+                    binarySearch.ReadTextFileAndSearch(filepath);
+                    break;
+                case 2:
+                    Anagram anagram = new Anagram();
+                    anagram.CheckAnagram();
+                    break;
+                case 3:
+                    flag = false;
+                    break;
+            }   
+        }
+    }
+}
